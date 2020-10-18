27d42f8b8f5b81851c7a024f6999c73c2c0f4e32
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-10-18 15:31:11 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan2/deklarasivariable/IF10K10119912Latihan2DeklarasiVariable.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan2.deklarasivariable;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * Deskripsi Program : Program ini berisi proses set dan get variabel
+ * 
+ */
+public class IF10K10119912Latihan2DeklarasiVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; // kondts uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variable
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();        
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " +  PHI);        
+        System.out.println("Isi variabel logika = " +  nilaiLogika);        
+        System.out.println("Isi variabel Karakter = " +  nilaiKarakter);
+
+
+
+        
+        
+        
+        
+        
+    }
+    
+}
